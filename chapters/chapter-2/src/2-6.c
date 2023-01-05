69393a84c6e1328470d782175ba2c6caf91a4f70
Kostya Farber <kostya.farber@gmail.com>
2023-01-05 08:51:00 +0000

    add setup for exercise 2-6

---
+++ chapters/chapter-2/src/2-6.c
@@ -0,0 +1,22 @@
+/*
+* setbits(x, p, n, y) that returns x
+* with n bits that begin at position p
+* set to the rightmost n bits of y, leaving
+* the other bits unchaged.
+*/
+
+#include <stdio.h>
+
+int setbits(int x, int p, int n, int y);
+
+int
+main(int argc, char** argv) {
+    
+    return 0;
+}
+
+int setbits(int x, int p, int n, int y) {
+    
+    
+    return x;
+}
